62ae003e326f3e8f36b8dec370a9721cff930797
Geoff Johnson <geoff.jay@gmail.com>
2024-06-22 13:14:29 -0700

    chore: drop existing wails

--- shell.nix
+++ shell.nix
@@ -58,6 +58,11 @@ in
 pkgs.mkShell {
   inherit (pre-commit-check) shellHook;
 
+  # shellHook = with pkgs; ''
+  #   export XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS;
+  #   export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/";
+  # '';
+
   packages = [
     goEnv
     gomod2nix
