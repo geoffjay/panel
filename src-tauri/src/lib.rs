3ac47cdd34b58a071db14b30f59b0a5988b45d64
Geoff Johnson <geoff.jay@gmail.com>
2024-12-02 09:06:18 -0800

    feat: add app events

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,59 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::fmt;
+use tauri::{AppHandle, ipc::Channel};
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+#[serde(rename_all = "camelCase", tag = "event", content = "data")]
+enum AppEvent<'a> {
+  #[serde(rename_all = "camelCase")]
+  LoadingStarted {
+    id: usize,
+  },
+  #[serde(rename_all = "camelCase")]
+  LoadingProgress {
+    id: usize,
+    percent: usize,
+    message: &'a str,
+  },
+  #[serde(rename_all = "camelCase")]
+  LoadingFinished {
+    id: usize,
+  },
+}
+
+impl<'a> fmt::Display for AppEvent<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AppEvent::LoadingStarted { id } => write!(f, "LoadingStarted(id: {})", id),
+            AppEvent::LoadingProgress { id, percent, message } => 
+                write!(f, "LoadingProgress(id: {}, {}%: {})", id, percent, message),
+            AppEvent::LoadingFinished { id } => write!(f, "LoadingFinished(id: {})", id),
+        }
+    }
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn initialize(_app: AppHandle, on_event: Channel<AppEvent>) {
+  let id = 1;
+
+  on_event.send(AppEvent::LoadingStarted { id }).unwrap();
+
+  for percent in [10, 30, 75, 90, 100] {
+    on_event.send(AppEvent::LoadingProgress { 
+      id,
+      percent: percent,
+      message: "Loading...",
+    }).unwrap();
+  }
+
+  on_event.send(AppEvent::LoadingFinished { id }).unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![initialize])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/features/home/page.tsx
+++ src/features/home/page.tsx
@@ -1,13 +1,46 @@
+import { useEffect } from "react";
+
+import { invoke, Channel } from "@tauri-apps/api/core";
+
 import { Layout } from "~components";
 import { Button } from "~components/ui/button";
 import { useMachineContext, EVENTS, STATES } from "~components/context/machine-provider";
 
+type AppEvent =
+  | {
+      event: "loading-started";
+      data: {
+        id: number;
+      };
+    }
+  | {
+      event: "loading-progress";
+      data: {
+        id: number;
+        percent: number;
+        message: string;
+      };
+    }
+  | {
+      event: "loading-finished";
+      data: {
+        id: number;
+      };
+    };
+
 const Page: React.FC = () => {
   const [current, send] = useMachineContext();
 
+  const onEvent = new Channel<AppEvent>();
+
+  onEvent.onmessage = (message) => {
+    console.log(`got app event ${message.event}`);
+  };
+
   const isLoading = current.name === STATES.LOADING;
 
-  const handleStartLoading = () => {
+  const handleStartLoading = async () => {
+    await invoke("initialize", { onEvent });
     send({ type: EVENTS.START_LOADING });
   };
 
